21cac47dfebf3fb8faae1617f01f41ce689b3a32
신민우 <minwoo@MacBook-Pro-3.local>
2022-09-20 20:44:35 +0900

    second

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const hostname = '127.0.0.1';   //로컬부스트 자기자신한테 요청
-const port = 3000;  //자기자신에게 오는 3000번 포트에만 응답
+const port = 4000;  //자기자신에게 오는 3000번 포트에만 응답
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
