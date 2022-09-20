cb273fe19813583523df72a3cbcb2392a417de71
신민우 <minwoo@MacBook-Pro-3.local>
2022-09-20 20:42:47 +0900

    first

---
+++ index.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';   //로컬부스트 자기자신한테 요청
+const port = 3000;  //자기자신에게 오는 3000번 포트에만 응답
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');    //헤더
+    res.end('Hello World\n');       //본문
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
+
