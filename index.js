5b868dea3c93e4ea7fbdc10b2a831e0ce5fe63ce
WhiteBurger <minwooshin123@gmail.com>
2022-09-26 18:46:59 +0900

    new

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 4000;  //자기자신에게 오는 3000번 포트에만 응답
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');    //헤더
-    res.end('Hello World\n');       //본문
+    res.end('Hello World hihi\n');       //본문
 });
 
 server.listen(port, hostname, () => {
